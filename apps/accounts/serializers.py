38f68d24f066ccd5511b85b9a84b50a196d43a4a
Aziz I. <zizzxiii@gmail.com>
2025-09-10 19:13:48 +0500

    Add user profile and update serializers with validation and methods

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,4 +1,5 @@
 from math import e
+import re
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
@@ -53,3 +54,69 @@ class UserLoginSerializer(serializers.Serializer):
             return attrs
         else:
             raise serializers.ValidationError({"detail": "Must include 'email' and 'password'."})
+        
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user profile.
+    """
+    full_name = serializers.ReadOnlyField()
+    posts_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'full_name', 'avatar', 'bio', 'created_at', 'updated_at', 'posts_count', 'comments_count')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+
+    def get_posts_count(self, obj):
+        return obj.posts.count()
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+    
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for updating user profile.
+    """
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'avatar', 'bio')
+    
+    def validate_avatar(self, value):
+        if value.size > 2 * 1024 * 1024:  # 2MB limit
+            raise serializers.ValidationError("Avatar size should not exceed 2MB.")
+        return value
+    
+    def update(self, instance, validated_data):
+        for  attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+    
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for changing user password.
+    """
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    new_password_confirm = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password_confirm']:
+            raise serializers.ValidationError({"new_password": "New password fields didn't match."})
+        return attrs
+    
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is not correct.")
+        return value
+    
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
