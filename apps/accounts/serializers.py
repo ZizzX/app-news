437a7d070247dc7fa43117af94e8b47bf7c326a9
Aziz I. <zizzxiii@gmail.com>
2025-09-10 19:01:09 +0500

    Implement custom User model and authentication serializers

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,55 @@
+from math import e
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration.
+    """
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password_confirm')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+
+class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login.
+    """
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        if email and password:
+            user = authenticate(request=self.context.get('request'), username=email, password=password)
+            if not user:
+                raise serializers.ValidationError({"detail": "Invalid credentials."})
+            if  not user.is_active:
+                raise serializers.ValidationError({"detail": "User account is disabled."})
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError({"detail": "Must include 'email' and 'password'."})
--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom User Model
-# AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
